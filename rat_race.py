c111942539071d9f45d0bf71e91786f5b6337681
christinamcmahon <christina.mcmahon1@gmail.com>
2018-01-07 16:19:23 +0700

    Wrote class Rat's methods

--- rat_race.py
+++ rat_race.py
@@ -36,7 +36,42 @@ RAT_2_CHAR = 'P'
 class Rat:
     """ A rat caught in a maze. """
 
-    # Write your Rat methods here.
+    def __init__(self, symbol, row, col):
+        ''' (Rat, str, int, int) -> NoneType
+        A Rat, represented with a 1-character symbol, at a location
+        represented by row and col. 
+        '''
+
+        self.symbol = symbol
+        self.row = row
+        self.col = col
+        num_sprouts_eaten = 0
+
+    def set_location(self, row, col):
+        ''' (Rat, int, int) -> NoneType
+        A Rat at a location represented by row and col. 
+        '''
+
+        self.row = row
+        self.col = col
+
+    def eat_sprout(self):
+        '''(Rat) -> NoneType
+        Adds one to the rat's num_sprouts_eaten.
+        '''
+
+        num_sprouts_eaten = num_sprouts_eaten + 1
+
+    def __str__(self):
+        '''(Rat) -> str
+        Return a string representation of the rat's symbol, location,
+        and number of sprouts eaten. 
+
+        >>> __str__(Rat)
+        'J at (4,3) ate 2 sprouts.'
+        '''
+
+        Return symbol + ' at (' + row + ',' + col + ') ate ' + num_sprouts_eaten + ' sprout(s).'
 
 class Maze:
     """ A 2D maze. """
